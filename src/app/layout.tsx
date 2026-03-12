32dfa6b30d5e3bbe4977e03203b1a68c7d2ba2a8
Tiago Ravi <tiagodesousacosta2024@gmail.com>
2026-03-12 18:49:16 -0300

    Adiciona verificação do Google Search Console

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,18 +4,23 @@ import { siteConfig } from "@/src/config/site";
 
 export const metadata: Metadata = {
   metadataBase: new URL(siteConfig.url),
+
   title: {
     default: siteConfig.defaultTitle,
     template: `%s | ${siteConfig.name}`,
   },
+
   description: siteConfig.defaultDescription,
+
   applicationName: siteConfig.name,
   authors: [{ name: siteConfig.companyName }],
   creator: siteConfig.companyName,
   publisher: siteConfig.companyName,
+
   alternates: {
     canonical: "/",
   },
+
   openGraph: {
     type: "website",
     locale: "pt_BR",
@@ -24,14 +29,20 @@ export const metadata: Metadata = {
     title: siteConfig.defaultTitle,
     description: siteConfig.defaultDescription,
   },
+
   twitter: {
     card: "summary_large_image",
     title: siteConfig.defaultTitle,
     description: siteConfig.defaultDescription,
   },
+
   robots: {
     index: true,
     follow: true,
+  },
+
+  verification: {
+    google: "e3uJXVUTNqrbOh4IA1TfTgt_zgm4DHlu3_CtLlfkRog",
   },
 };
 
